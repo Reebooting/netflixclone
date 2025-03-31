5875df85b8fbe0779881b00667a6e343297e4d0b
Reebooting <87072968+Reebooting@users.noreply.github.com>
2025-03-31 20:47:46 +0530

    Update server.js

--- netflix_backend/server.js
+++ netflix_backend/server.js
@@ -3,13 +3,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB Atlas Connection
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = "mongodb+srv://<new-user69>:<Suraj2025>@cluster0.mongodb.net/users?retryWrites=true&w=majority";
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("✅ MongoDB Connected Successfully!"))
     .catch(err => console.log("❌ MongoDB Connection Error: ", err));
