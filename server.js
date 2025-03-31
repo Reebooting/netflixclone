c043d48168f8f1ad475bc743a345e74b3a116286
Reebooting <87072968+Reebooting@users.noreply.github.com>
2025-03-31 20:24:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const MONGO_URI = "mongodb+srv://<new-user69>:<Suraj2025>@cluster0.1ct8eyx.mongodb.net/users";
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB Atlas Connection
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = "mongodb+srv://<new-user69>:<Suraj2025>@cluster0.1ct8eyx.mongodb.net/users";
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("✅ MongoDB Connected Successfully!"))
     .catch(err => console.log("❌ MongoDB Connection Error: ", err));
